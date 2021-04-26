97cd378e27eeda358e044c914b6d1ede67fa9012
perhostenherra <77660081+perhostenherra@users.noreply.github.com>
2021-04-26 13:51:20 +0300

    Create IPersonView.cs

---
+++ IPersonView.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonDBApp.Views
+{
+    public interface IPersonView
+    {
+        void CreatePerson();
+        void PrintAllPeople();
+        void PrintSinglePerson();
+        void PrintByCity();
+
+        void UpdatePerson();
+
+        void DeletePerson();
+    }
+}
