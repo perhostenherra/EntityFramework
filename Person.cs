05b7e99bfd057e45e78972585c8b2d3cee18eec7
perhostenherra <77660081+perhostenherra@users.noreply.github.com>
2021-04-26 13:54:48 +0300

    Create Person.cs

---
+++ Person.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace PersonDBApp.Models
+{
+    public partial class Person
+    {
+        public Person()
+        {
+            Shoes = new HashSet<Shoe>();
+        }
+
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string Sex { get; set; }
+        public string EyeColor { get; set; }
+        public int? ShoeSize { get; set; }
+        public int? Height { get; set; }
+
+        public virtual ICollection<Shoe> Shoes { get; set; }
+    }
+}
