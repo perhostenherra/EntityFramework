630b06c1d106970404c876592907d4f53a04cd9d
perhostenherra <77660081+perhostenherra@users.noreply.github.com>
2021-04-26 13:52:32 +0300

    Create IPersonService.cs

---
+++ IPersonService.cs
@@ -0,0 +1,21 @@
+using PersonDBApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonDBApp.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        List<Person> Read();
+
+        Person Read(long id);
+
+        List<Person> Read(string city);
+
+        Person Update(long id, Person person);
+        void Delete(long id);
+
+    }
+}
